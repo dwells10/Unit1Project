8bcbc88e9255cd97c02a44a0c76817f54331d119
dillonwells <dwells10@my.wcyc.edu>
2018-03-06 13:24:17 -0600

    Initial Project Commit

--- src/com/wellztech/Main.java
+++ src/com/wellztech/Main.java
@@ -1,8 +1,38 @@
 package com.wellztech;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //csv Comma seperated values.
+
+        String fileName1 = "places.csv";
+        File file1 = new File(fileName1);
+        String[] datas;
+
+        try {
+
+            Scanner inputStream = new Scanner(file1);
+            while (inputStream.hasNext()){
+
+                String data = inputStream.nextLine();
+                System.out.println(data);
+
+
+            }
+
+            inputStream.close();
+
+        } catch (FileNotFoundException e){
+
+            e.printStackTrace();
+
+        }
+
+
+
+
     }
 }
